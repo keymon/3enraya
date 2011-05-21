aee98d74aedef864f22ec1b938ddebecbe2267ec
Juan Penalta <jpenalta@gmail.com>
2011-05-21 18:15:32 +0200

    primeros cambios grupo 1

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,30 @@
 package controllers;
 
 import play.*;
+import play.data.validation.Required;
+import play.data.validation.Email;
+import play.data.validation.Equals;
 import play.mvc.*;
 
 import java.util.*;
 
-import models.*;
+import models.Usuario;
 
 public class Application extends Controller {
 
-    public static void index() {
-        render();
-    }
+	public static void index() {
+		render();
+	}
+
+	public static void registrarUsuario(@Required String login,
+			@Required String password, @Required @Equals("password") String passwordRepetido,
+			@Email @Required String correoElectronico) {
+	    if (!validation.hasErrors()) {
+	    	new Usuario(login, password, correoElectronico); 
+	        render("Application/envioCorreoConfirmacion.html");
+	    }
+
+		render();
+	}
 
 }
